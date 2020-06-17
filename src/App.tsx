91a01f3a0f28a9b54b5e6b4b8d2e21a7012f5616
vikgulb <vikgulb@gmail.com>
2020-06-17 13:10:32 +0300

    set up tailwind.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Quotebox from "./components/Quotebox";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen bg-red-100">
+      <Quotebox />
     </div>
   );
 }
---
+++ src/components/Quotebox.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Quotebox = () => {
+  return <div>i am quotebox</div>;
+};
+
+export default Quotebox;
